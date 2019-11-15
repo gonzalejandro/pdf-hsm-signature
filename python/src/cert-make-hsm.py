8053632a080236d616df0145e96809d6136257eb
gonzalejandro <alejandro.ge@tutanota.com>
2019-11-15 12:30:09 -0300

    updated hsm sign according to last endesive update

--- python/src/cert-make-hsm.py
+++ python/src/cert-make-hsm.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/env vpython3
 # coding: utf-8
 
 import os
 import sys
-from endesive import hsm
-import PyKCS11
 
-cwd = os.getcwd()
 os.environ['SOFTHSM2_CONF'] = 'softhsm2.conf'
-if not os.path.exists(os.path.join(cwd, 'softhsm2.conf')):
-    with open('softhsm2.conf', "wt") as conf:
-        conf.write(
-            f'''\
-            log.level = DEBUG
-            directories.tokendir = {cwd}/softhsm2/
-            objectstore.backend = file
-            slots.removable = false
-            '''
-        )
+if not os.path.exists(os.path.join(os.getcwd(), 'softhsm2.conf')):
+    open('softhsm2.conf', 'wt').write('''\
+log.level = DEBUG
+directories.tokendir = %s/softhsm2/
+objectstore.backend = file
+slots.removable = false
+''' % os.getcwd())
+if not os.path.exists(os.path.join(os.getcwd(), 'softhsm2')):
+    os.mkdir(os.path.join(os.getcwd(), 'softhsm2'))
 
 #
 #!/bin/bash
@@ -26,25 +22,37 @@ if not os.path.exists(os.path.join(cwd, 'softhsm2.conf')):
 #softhsm2-util --show-slots
 #
 
-dllpath = '/usr/lib/softhsm/libsofthsm2.so'
+if sys.platform == 'win32':
+    dllpath = r'W:\binw\SoftHSM2\lib\softhsm2-x64.dll'
+else:
+    dllpath = '/usr/lib/libsofthsm2.so'
 
+from endesive import hsm
+import PyKCS11 as PK11
 
+'''
+Create two certificates:
+1. self signed CA certificate with serial equal to HSM keyID=0x01
+2. USER 1 certificate with serial equal to HSM keyID=0x666690
+'''
 class HSM(hsm.HSM):
     def main(self):
         cakeyID = bytes((0x1,))
-        rec = self.session.findObjects([(PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE), (PyKCS11.CKA_ID, cakeyID)])
+        rec = self.session.findObjects([(PK11.CKA_CLASS, PK11.CKO_PRIVATE_KEY), (PK11.CKA_ID, cakeyID)])
         if len(rec) == 0:
             label = 'ca'
             self.gen_privkey(label, cakeyID)
             self.ca_gen(label, cakeyID, 'CA')
 
-        keyID = bytes((0x66, 0x66, 0x90))
-        rec = self.session.findObjects([(PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY), (PyKCS11.CKA_ID, keyID)])
+        keyID = bytes((0x66,0x66,0x90))
+        rec = self.session.findObjects([(PK11.CKA_CLASS, PK11.CKO_PRIVATE_KEY), (PK11.CKA_ID, keyID)])
         if len(rec) == 0:
             label = 'user 1'
             self.gen_privkey(label, keyID)
             self.ca_sign(keyID, label, 0x666690, "USER 1", 365, cakeyID)
 
+        #self.cert_export('cert-ca', cakeyID)
+        #self.cert_export('cert-user1', keyID)
 
 def main():
     cls = HSM(dllpath)
@@ -54,7 +62,4 @@ def main():
         cls.main()
     finally:
         cls.logout()
-
-
-if __name__ == "__main__":
-    main()
+main()
